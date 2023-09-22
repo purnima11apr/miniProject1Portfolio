16a4ea18ebb678cf7fa56a5f23c143d971c906f9
purnima11apr <purnimachdry@gmail.com>
2023-09-22 21:00:24 +0530

    Javascript practice

---
+++ purnima.js
@@ -0,0 +1,128 @@
+console.log("Hello, JavaScript!");
+
+/*let str = "abc";
+let num = 11;
+console.log(str+num);
+
+console.log(typeof (str+num));
+
+const obj = {
+    name : "Aman",
+    section : 1,
+    job : false,
+
+}
+console.log(obj);
+
+obj["husband"] = "Yes";
+
+console.log(obj);
+
+const dictionary= {
+    appreciate :"to enjoy something or to understand the value of somebody/something",
+    Ataraxia : "calmness untroubled by mental or emotional disquiet." ,
+
+}
+
+console.log(dictionary.Ataraxia);
+
+let age = prompt("write your age");
+if(age>10 && age<20){
+    console.log("perfect age");
+}else {
+    console.log("not perfect");
+}
+
+let day = prompt("day");
+switch (parseInt(day)){
+    case 1 : console.log("Monday");
+    break;
+    case 2 : console.log("Tuesday");
+    break;
+    case 3 : console.log("Wednesday");
+    break;
+    case 4 : console.log("Thursday");
+    break;
+    case 5 : console.log("Friday");
+    break;
+    case 6 : console.log("Saturday");
+    break;
+    case 7 : console.log("Sunday");
+    break;
+    default : console.log("invalid");
+}
+
+
+let num = prompt("enter number");
+num = Number.parseInt(num);
+
+if(num%2==0 && num%3==0){
+    console.log("divisible by 2 and 3");
+}else {
+    console.log("No it is not divisible");
+}
+
+if(num%2==0 || num%3==0){
+    console.log("divisible");
+}else {
+    console.log("No it is not divisible");
+}
+let age = prompt("age");
+let a = age>18 ? "You can Drive" : "you cannot drive"
+console.log(a);
+
+let marks = {
+    Aman : 95,
+    Griffi : 80,
+    Purnima: 93,
+    Vaibhav: 94,
+}
+
+for(i in marks) {
+    console.log(i + " are " + marks[i]);
+}
+
+let correct = 11;
+let i ;
+while (i!=correct){
+    console.log("try again")
+    i = prompt("Enter")
+}
+
+console.log("correct");
+
+const mean = (a , b , c, d) => {
+
+    return (a+b+c+d)/4;
+}
+
+console.log(mean(11,22,30,12));
+
+const sentence = "Aman is My Husband";
+const word = "Husband";
+
+console.log(sentence.includes(word));
+console.log(sentence.startsWith("A"));
+console.log(sentence.endsWith("D"));
+
+let name = "AmAn";
+
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+
+let str = "Please give me Rs 1999"
+
+let amt = str.slice(15);
+console.log(amt);
+
+let color = prompt("color");
+
+document.body.style.background = color;
+
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red";
+document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green";
+
+Array.from(document.getElementsByTagName("li")).forEach(element =>
+    {
+        element.style.background = "cyan";
+    })*/
